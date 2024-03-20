92222966ad87658b5eadd680f834b2906700dba4
tamarakokhan <tamara.n.kokhan@gmail.com>
2024-03-20 23:15:25 +0300

    внесла последние правки в программу

--- pr/Program.cs
+++ pr/Program.cs
@@ -2,7 +2,6 @@
 // формирует новый массив из строк, длина которых меньше, либо равна 3 символам.
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами. 
-// Tom, Sam, Bobby, Teddy, Jon
 
 int Input(string text)
 {
@@ -16,14 +15,45 @@ string InputString(string text)
     return Console.ReadLine();
 }
 
-void FillArray(string[] array)
+string[] CreateArray(string text)
 {
+    string[] array = new string[Input(text)];
     int index = 0;
     while (index < array.Length)
     {
         array[index] = InputString("Введите элемент массива: ");
         index++;
     }
+    return array;
+}
+
+string[] NewArray(string[] array)
+{
+    int i = 0;
+    int count = 0;
+    while (i < array.Length)
+    {
+        if (array[i].Length <= 3)
+        {
+            count++;
+        }
+        i++;
+    }
+
+    string[] newArray = new string[count]; ;
+
+    int k = 0;
+    int q = 0;
+    while (k < array.Length)
+    {
+        if (array[k].Length <= 3)
+        {
+            newArray[q] = array[k];
+            q++;
+        }
+        k++;
+    }
+    return newArray;
 }
 
 void PrintArray(string[] array)
@@ -36,34 +66,10 @@ void PrintArray(string[] array)
     }
 }
 
-string[] array1 = new string[Input("Введите размер массива: ")];
-FillArray(array1);
+string[] initialArray = CreateArray("Введите размер массива: ");
 
-int i = 0;
-int count = 0;
-while (i < array1.Length)
-{
-    if (array1[i].Length <= 3)
-    {
-        count++;
-    }
-    i++;
-}
-
-string[] array2 = new string[count]; ;
-
-int k = 0;
-int q = 0;
-while (k < array1.Length)
-{
-    if (array1[k].Length <= 3)
-    {
-        array2[q] = array1[k];
-        q++;
-    }
-    k++;
-}
-
-PrintArray(array1);
+Console.Write("Первоначальный массив: ");
+PrintArray(initialArray);
 Console.WriteLine();
-PrintArray(array2);
+Console.Write("Новый массив: ");
+PrintArray(NewArray(initialArray));
